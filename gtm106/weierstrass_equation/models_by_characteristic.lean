9974bfad2945b9dcba812a9d6926ac9d70f0397c
acmepjz <acme_pjz@hotmail.com>
2021-04-22 08:17:02 +0800

    added experimental ring_char2 and ring_char3 tactics and used them

--- gtm106/weierstrass_equation/models_by_characteristic.lean
+++ gtm106/weierstrass_equation/models_by_characteristic.lean
@@ -13,72 +13,62 @@ def weierstrass_equation.is_model_of_char_neq_2 {K : Type*} [field K]
 (E : weierstrass_equation K) :=
 E.a1 = 0 ∧ E.a3 = 0
 
+@[simp]
 lemma weierstrass_equation.b2_of_model_of_char_neq_2 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2) :
 E.b2 = 4*E.a2 :=
 begin
-  unfold weierstrass_equation.b2,
-  rw h.1,
-  ring,
+  simp [weierstrass_equation.b2, h.1],
 end
 
+@[simp]
 lemma weierstrass_equation.b4_of_model_of_char_neq_2 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2) :
 E.b4 = 2*E.a4 :=
 begin
-  unfold weierstrass_equation.b4,
-  rw [h.1, h.2],
-  ring,
+  simp [weierstrass_equation.b4, h.1, h.2],
 end
 
+@[simp]
 lemma weierstrass_equation.b6_of_model_of_char_neq_2 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2) :
 E.b6 = 4*E.a6 :=
 begin
-  unfold weierstrass_equation.b6,
-  rw h.2,
-  ring,
+  simp [weierstrass_equation.b6, h.2],
 end
 
+@[simp]
 lemma weierstrass_equation.b8_of_model_of_char_neq_2 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2) :
 E.b8 = 4*E.a2*E.a6 - E.a4^2 :=
 begin
-  unfold weierstrass_equation.b8,
-  rw [h.1, h.2],
-  ring,
+  simp [weierstrass_equation.b8, h.1, h.2, zero_pow],
 end
 
+@[simp]
 lemma weierstrass_equation.c4_of_model_of_char_neq_2 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2) :
 E.c4 = 16*E.a2^2 - 48*E.a4 :=
 begin
-  unfold weierstrass_equation.c4,
-  rw E.b2_of_model_of_char_neq_2 h,
-  rw E.b4_of_model_of_char_neq_2 h,
+  simp [weierstrass_equation.c4, h],
   ring,
 end
 
+@[simp]
 lemma weierstrass_equation.c6_of_model_of_char_neq_2 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2) :
 E.c6 = -64*E.a2^3 + 288*E.a2*E.a4 - 864*E.a6 :=
 begin
-  unfold weierstrass_equation.c6,
-  rw E.b2_of_model_of_char_neq_2 h,
-  rw E.b4_of_model_of_char_neq_2 h,
-  rw E.b6_of_model_of_char_neq_2 h,
+  simp [weierstrass_equation.c6, h],
   ring,
 end
 
+@[simp]
 lemma weierstrass_equation.disc_of_model_of_char_neq_2 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2) :
 E.disc = -64*E.a2^3*E.a6 + 16*E.a2^2*E.a4^2 - 64*E.a4^3 - 432*E.a6^2 +288*E.a2*E.a4*E.a6 :=
 begin
-  unfold weierstrass_equation.disc,
-  rw E.b2_of_model_of_char_neq_2 h,
-  rw E.b4_of_model_of_char_neq_2 h,
-  rw E.b6_of_model_of_char_neq_2 h,
-  rw E.b8_of_model_of_char_neq_2 h,
+  simp [weierstrass_equation.disc, h],
   ring,
 end
 
@@ -106,72 +96,65 @@ def weierstrass_equation.is_model_of_char_neq_2_and_3 {K : Type*} [field K]
 (E : weierstrass_equation K) :=
 E.a1 = 0 ∧ E.a2 = 0 ∧ E.a3 = 0
 
+@[simp]
 lemma weierstrass_equation.b2_of_model_of_char_neq_2_and_3 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2_and_3) :
 E.b2 = 0 :=
 begin
-  unfold weierstrass_equation.b2,
-  rw [h.1, h.2.1],
-  ring,
+  simp [weierstrass_equation.b2, h.1, h.2.1],
 end
 
+@[simp]
 lemma weierstrass_equation.b4_of_model_of_char_neq_2_and_3 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2_and_3) :
 E.b4 = 2*E.a4 := E.b4_of_model_of_char_neq_2 ⟨ h.1, h.2.2 ⟩
 
+@[simp]
 lemma weierstrass_equation.b6_of_model_of_char_neq_2_and_3 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2_and_3) :
 E.b6 = 4*E.a6 := E.b6_of_model_of_char_neq_2 ⟨ h.1, h.2.2 ⟩
 
+@[simp]
 lemma weierstrass_equation.b8_of_model_of_char_neq_2_and_3 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2_and_3) :
 E.b8 = -E.a4^2 :=
 begin
-  unfold weierstrass_equation.b8,
-  rw [h.1, h.2.1, h.2.2],
-  ring,
+  simp [weierstrass_equation.b8, h.1, h.2.1, h.2.2, zero_pow],
 end
 
+@[simp]
 lemma weierstrass_equation.c4_of_model_of_char_neq_2_and_3 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2_and_3) :
 E.c4 = -48*E.a4 :=
 begin
-  unfold weierstrass_equation.c4,
-  rw E.b2_of_model_of_char_neq_2_and_3 h,
-  rw E.b4_of_model_of_char_neq_2_and_3 h,
+  simp [weierstrass_equation.c4, h],
   ring,
 end
 
+@[simp]
 lemma weierstrass_equation.c6_of_model_of_char_neq_2_and_3 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2_and_3) :
 E.c6 = -864*E.a6 :=
 begin
-  unfold weierstrass_equation.c6,
-  rw E.b2_of_model_of_char_neq_2_and_3 h,
-  rw E.b4_of_model_of_char_neq_2_and_3 h,
-  rw E.b6_of_model_of_char_neq_2_and_3 h,
+  simp [weierstrass_equation.c6, h],
   ring,
 end
 
+@[simp]
 lemma weierstrass_equation.disc_of_model_of_char_neq_2_and_3 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2_and_3) :
 E.disc = -16*(4*E.a4^3 + 27*E.a6^2) :=
 begin
-  unfold weierstrass_equation.disc,
-  rw E.b2_of_model_of_char_neq_2_and_3 h,
-  rw E.b4_of_model_of_char_neq_2_and_3 h,
-  rw E.b6_of_model_of_char_neq_2_and_3 h,
-  rw E.b8_of_model_of_char_neq_2_and_3 h,
+  simp [weierstrass_equation.disc, h],
   ring,
 end
 
+@[simp]
 lemma weierstrass_equation.j_of_model_of_char_neq_2_and_3 {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2_and_3) (hchar2 : ring_char K ≠ 2) :
 E.j = 6912*E.a4^3/(4*E.a4^3 + 27*E.a6^2) :=
 begin
-  unfold weierstrass_equation.j,
-  rw E.disc_of_model_of_char_neq_2_and_3 h,
-  rw E.c4_of_model_of_char_neq_2_and_3 h,
+  simp [weierstrass_equation.j, h],
   by_cases h : 4*E.a4^3 + 27*E.a6^2 = 0, {
     rw h, simp,
   },
@@ -210,42 +193,33 @@ def weierstrass_equation.is_model_of_char_3_j_non_zero {K : Type*} [field K]
 (E : weierstrass_equation K) :=
 E.a1 = 0 ∧ E.a3 = 0 ∧ E.a4 = 0
 
+@[simp]
 lemma weierstrass_equation.c4_of_model_of_char_3_j_non_zero {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_3_j_non_zero) (hchar3 : ring_char K = 3) :
 E.c4 = E.a2^2 :=
 begin
-  unfold weierstrass_equation.c4
-  weierstrass_equation.b2
-  weierstrass_equation.b4,
-  rw [h.1, h.2.1, h.2.2],
-  simp [zero_pow], ring,
-  have h := cong_char_is_eq hchar3 16 1 (by norm_num), norm_cast at h, rw h, clear h,
-  ring,
+  simp [weierstrass_equation.c4, weierstrass_equation.b2, weierstrass_equation.b4,
+    h.1, h.2.1, h.2.2, zero_pow],
+  ring_char3,
 end
 
+@[simp]
 lemma weierstrass_equation.disc_of_model_of_char_3_j_non_zero {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_3_j_non_zero) (hchar3 : ring_char K = 3) :
 E.disc = -E.a2^3*E.a6 :=
 begin
-  unfold weierstrass_equation.disc
-  weierstrass_equation.b2
-  weierstrass_equation.b4
-  weierstrass_equation.b6
-  weierstrass_equation.b8,
-  rw [h.1, h.2.1, h.2.2],
-  simp [zero_pow], ring,
-  have h := cong_char_is_eq hchar3 64 1 (by norm_num), norm_cast at h, rw h, clear h,
-  have h := dvd_char_is_zero hchar3 432 (by norm_num), norm_cast at h, rw h, clear h,
-  ring,
+  simp [weierstrass_equation.disc, weierstrass_equation.b2, weierstrass_equation.b4,
+    weierstrass_equation.b6, weierstrass_equation.b8,
+    h.1, h.2.1, h.2.2, zero_pow],
+  ring_char3,
 end
 
+@[simp]
 lemma weierstrass_equation.j_of_model_of_char_3_j_non_zero {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_3_j_non_zero) (hchar3 : ring_char K = 3) :
 E.j = -E.a2^3/E.a6 :=
 begin
-  unfold weierstrass_equation.j,
-  rw E.disc_of_model_of_char_3_j_non_zero h hchar3,
-  rw E.c4_of_model_of_char_3_j_non_zero h hchar3,
+  simp [weierstrass_equation.j, h, hchar3],
   by_cases ha6 : E.a6 = 0, {
     rw ha6, simp,
   },
@@ -255,33 +229,30 @@ begin
   field_simp [ha2, ha6], ring,
 end
 
+@[simp]
 lemma weierstrass_equation.c4_of_model_of_char_3_j_zero {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2_and_3) (hchar3 : ring_char K = 3) :
 E.c4 = 0 :=
 begin
-  rw E.c4_of_model_of_char_neq_2_and_3 h,
-  have h := dvd_char_is_zero hchar3 48 (by norm_num), norm_cast at h, rw h, clear h,
-  ring,
+  simp [h],
+  ring_char3,
 end
 
+@[simp]
 lemma weierstrass_equation.disc_of_model_of_char_3_j_zero {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2_and_3) (hchar3 : ring_char K = 3) :
 E.disc = -E.a4^3 :=
 begin
-  rw E.disc_of_model_of_char_neq_2_and_3 h,
-  have h := dvd_char_is_zero hchar3 27 (by norm_num), norm_cast at h, rw h, clear h,
-  simp, rw ← mul_assoc, norm_num,
-  have h := cong_char_is_eq hchar3 64 1 (by norm_num), norm_cast at h, rw h, clear h,
-  ring,
+  simp [h],
+  ring_char3,
 end
 
+@[simp]
 lemma weierstrass_equation.j_of_model_of_char_3_j_zero {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_neq_2_and_3) (hchar3 : ring_char K = 3) :
 E.j = 0 :=
 begin
-  unfold weierstrass_equation.j,
-  rw E.c4_of_model_of_char_3_j_zero h hchar3,
-  ring,
+  simp [weierstrass_equation.j, h, hchar3],
 end
 
 lemma weierstrass_equation.have_model_of_char_3 {K : Type*} [field K]
@@ -295,18 +266,12 @@ begin
   rcases E.have_model_of_char_neq_2 hchar2 with ⟨ C, h1 ⟩,
   set E' := C.change_curve E with hE,
   have hdisc : E'.disc = E'.a2^2*E'.a4^2 - E'.a2^3*E'.a6 - E'.a4^3 := by {
-    rw E'.disc_of_model_of_char_neq_2 h1,
-    have h := cong_char_is_eq hchar3 64 1 (by norm_num), norm_cast at h, rw h, clear h,
-    have h := cong_char_is_eq hchar3 16 1 (by norm_num), norm_cast at h, rw h, clear h,
-    have h := dvd_char_is_zero hchar3 288 (by norm_num), norm_cast at h, rw h, clear h,
-    have h := dvd_char_is_zero hchar3 432 (by norm_num), norm_cast at h, rw h, clear h,
-    ring,
+    simp [h1],
+    ring_char3,
   },
   have hc4 : E'.c4 = E'.a2^2 := by {
-    rw E'.c4_of_model_of_char_neq_2 h1,
-    have h := dvd_char_is_zero hchar3 48 (by norm_num), norm_cast at h, rw h, clear h,
-    have h := cong_char_is_eq hchar3 16 1 (by norm_num), norm_cast at h, rw h, clear h,
-    ring,
+    simp [h1],
+    ring_char3,
   },
   have hj : E'.j = E.j := C.j E,
   have hnonsing : E.non_singular' ↔ E'.non_singular' := C.preserve_non_singular' E,
@@ -341,9 +306,7 @@ begin
   rw [h1.1, h1.2],
   split, { ring, }, split, { ring, },
   field_simp [ha2],
-  ring,
-  have h := dvd_char_is_zero hchar3 3 (by norm_num), norm_cast at h, rw h, clear h,
-  ring,
+  ring_char3,
 end
 
 def weierstrass_equation.is_model_of_char_2_j_non_zero {K : Type*} [field K]
@@ -354,89 +317,62 @@ def weierstrass_equation.is_model_of_char_2_j_zero {K : Type*} [field K]
 (E : weierstrass_equation K) :=
 E.a1 = 0 ∧ E.a2 = 0
 
+@[simp]
 lemma weierstrass_equation.c4_of_model_of_char_2_j_non_zero {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_2_j_non_zero) (hchar2 : ring_char K = 2) :
 E.c4 = 1 :=
 begin
-  unfold weierstrass_equation.c4
-  weierstrass_equation.b2
-  weierstrass_equation.b4,
-  rw [h.1, h.2.1, h.2.2],
-  simp [zero_pow],
-  have h := dvd_char_is_zero hchar2 4 (by norm_num), norm_cast at h, rw h, clear h,
-  ring,
+  simp [weierstrass_equation.c4, weierstrass_equation.b2, weierstrass_equation.b4,
+    h.1, h.2.1, h.2.2, zero_pow],
+  ring_char2,
 end
 
+@[simp]
 lemma weierstrass_equation.disc_of_model_of_char_2_j_non_zero {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_2_j_non_zero) (hchar2 : ring_char K = 2) :
 E.disc = E.a6 :=
-begin
-  unfold weierstrass_equation.disc
-  weierstrass_equation.b2
-  weierstrass_equation.b4
-  weierstrass_equation.b6
-  weierstrass_equation.b8,
-  rw [h.1, h.2.1, h.2.2],
-  simp [zero_pow], ring,
-  have h := dvd_char_is_zero hchar2 432 (by norm_num), norm_cast at h, rw h, clear h,
-  have h := dvd_char_is_zero hchar2 64 (by norm_num), norm_cast at h, rw h, clear h,
-  have h := dvd_char_is_zero hchar2 48 (by norm_num), norm_cast at h, rw h, clear h,
-  have h := dvd_char_is_zero hchar2 12 (by norm_num), norm_cast at h, rw h, clear h,
-  ring,
-  calc -E.a6 = E.a6 - 2*E.a6 : by ring
-  ... = E.a6 : by {
-    have h := dvd_char_is_zero hchar2 2 (by norm_num), norm_cast at h, rw h, clear h,
-    ring,
-  },
+sub_eq_zero.1 begin
+  simp [weierstrass_equation.disc, weierstrass_equation.b2, weierstrass_equation.b4,
+    weierstrass_equation.b6, weierstrass_equation.b8,
+    h.1, h.2.1, h.2.2, zero_pow],
+  ring_char2,
 end
 
+@[simp]
 lemma weierstrass_equation.j_of_model_of_char_2_j_non_zero {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_2_j_non_zero) (hchar2 : ring_char K = 2) :
 E.j = 1/E.a6 :=
 begin
-  unfold weierstrass_equation.j,
-  rw E.disc_of_model_of_char_2_j_non_zero h hchar2,
-  rw E.c4_of_model_of_char_2_j_non_zero h hchar2,
-  ring,
+  simp [weierstrass_equation.j, h, hchar2],
 end
 
+@[simp]
 lemma weierstrass_equation.c4_of_model_of_char_2_j_zero {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_2_j_zero) (hchar2 : ring_char K = 2) :
 E.c4 = 0 :=
 begin
-  unfold weierstrass_equation.c4
-  weierstrass_equation.b2
-  weierstrass_equation.b4,
-  rw [h.1, h.2],
-  have h := dvd_char_is_zero hchar2 24 (by norm_num), norm_cast at h, rw h, clear h,
-  simp [zero_pow],
+  simp [weierstrass_equation.c4, weierstrass_equation.b2, weierstrass_equation.b4,
+    h.1, h.2, zero_pow],
+  ring_char2,
 end
 
+@[simp]
 lemma weierstrass_equation.disc_of_model_of_char_2_j_zero {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_2_j_zero) (hchar2 : ring_char K = 2) :
 E.disc = E.a3^4 :=
-begin
-  unfold weierstrass_equation.disc
-  weierstrass_equation.b2
-  weierstrass_equation.b4
-  weierstrass_equation.b6
-  weierstrass_equation.b8,
-  rw [h.1, h.2],
-  simp [zero_pow], ring,
-  have h := dvd_char_is_zero hchar2 64 (by norm_num), norm_cast at h, rw h, clear h,
-  have h := dvd_char_is_zero hchar2 432 (by norm_num), norm_cast at h, rw h, clear h,
-  have h := dvd_char_is_zero hchar2 216 (by norm_num), norm_cast at h, rw h, clear h,
-  have h := cong_char_is_eq hchar2 27 (-1) (by norm_num), norm_cast at h, rw h, clear h,
-  ring, simp,
+sub_eq_zero.1 begin
+  simp [weierstrass_equation.disc, weierstrass_equation.b2, weierstrass_equation.b4,
+    weierstrass_equation.b6, weierstrass_equation.b8,
+    h.1, h.2, zero_pow],
+  ring_char2,
 end
 
+@[simp]
 lemma weierstrass_equation.j_of_model_of_char_2_j_zero {K : Type*} [field K]
 (E : weierstrass_equation K) (h : E.is_model_of_char_2_j_zero) (hchar2 : ring_char K = 2) :
 E.j = 0 :=
 begin
-  unfold weierstrass_equation.j,
-  rw E.c4_of_model_of_char_2_j_zero h hchar2,
-  ring,
+  simp [weierstrass_equation.j, h, hchar2],
 end
 
 lemma weierstrass_equation.have_model_of_char_2 {K : Type*} [field K]
@@ -445,16 +381,8 @@ lemma weierstrass_equation.have_model_of_char_2 {K : Type*} [field K]
 ∨ (E.j = 0 ∧ ∃ (C : linear_change_of_variable K), (C.change_curve E).is_model_of_char_2_j_zero) :=
 begin
   have hc4 : E.c4 = E.a1^4 := by {
-    unfold weierstrass_equation.c4
-    weierstrass_equation.b2
-    weierstrass_equation.b4
-    weierstrass_equation.a1
-    weierstrass_equation.a2
-    weierstrass_equation.a3
-    weierstrass_equation.a4,
-    have h := dvd_char_is_zero hchar2 4 (by norm_num), norm_cast at h, rw h, clear h,
-    have h := dvd_char_is_zero hchar2 24 (by norm_num), norm_cast at h, rw h, clear h,
-    ring,
+    simp [weierstrass_equation.c4, weierstrass_equation.b2, weierstrass_equation.b4],
+    ring_char2,
   },
   unfold weierstrass_equation.j,
   rw hc4,
@@ -474,8 +402,7 @@ begin
     linear_change_of_variable.s
     linear_change_of_variable.t,
     rw ha1,
-    have h := cong_char_is_eq hchar2 3 (-1) (by norm_num), norm_cast at h, rw h, clear h,
-    split, { ring, }, simp,
+    ring_char2,
   },
   left,
   split, {
@@ -495,11 +422,7 @@ begin
   linear_change_of_variable.t,
   split, { field_simp [ha1], },
   split, {
-    field_simp [ha1], ring,
-    have h := dvd_char_is_zero hchar2 2 (by norm_num), norm_cast at h, rw h, clear h,
-    ring,
+    field_simp [ha1], ring_char2,
   },
-  field_simp [pow_succ, ha1], ring,
-  have h := dvd_char_is_zero hchar2 2 (by norm_num), norm_cast at h, rw h, clear h,
-  ring,
+  field_simp [pow_succ, ha1], ring_char2,
 end
--- myhelper/char.lean
+++ myhelper/char.lean
@@ -6,13 +6,13 @@ import algebra.char_zero
 import algebra.char_p
 import tactic
 
-lemma char_is_prime_or_zero' (K : Type*) [field K] : nat.prime (ring_char K) ∨ ring_char K = 0 :=
+lemma char_is_prime_or_zero'
+(R : Type*) [semiring R] [no_zero_divisors R] [nontrivial R]
+: nat.prime (ring_char R) ∨ ring_char R = 0 :=
 begin
-  let p := ring_char K,
-  have h : ring_char K = p := rfl,
-  rw h,
+  set p := ring_char R with h,
   rw ring_char.eq_iff at h,
-  exact char_p.char_is_prime_or_zero K p,
+  exact char_p.char_is_prime_or_zero R p,
 end
 
 example : char_zero ℚ :=
@@ -22,57 +22,213 @@ end
 
 example : ring_char ℚ = 0 :=
 begin
-  have : char_p ℚ 0 := char_p.of_char_zero ℚ,
-  have := ring_char.eq ℚ this,
-  exact this.symm,
+  rw ring_char.eq_iff,
+  exact char_p.of_char_zero ℚ,
 end
 
 lemma prime_neq_char_is_non_zero
-(K : Type*) [field K] (q : ℕ) (hq : nat.prime q) (hneq : ring_char K ≠ q) : (q : K) ≠ 0 :=
+(R : Type*) [semiring R] [nontrivial R]
+(q : ℕ) (hq : nat.prime q) (hneq : ring_char R ≠ q) : (q : R) ≠ 0 :=
 begin
   intro h0,
   replace h0 := ring_char.dvd h0,
-  set p := ring_char K with ← hp,
-  rw ring_char.eq_iff at hp,
-  cases char_p.char_is_prime_or_zero K p with hp' hp', {
-    rw nat.prime_dvd_prime_iff_eq hp' hq at h0,
-    exact hneq h0,
+  cases (nat.dvd_prime hq).1 h0 with hp' hp', {
+    have : (1:R) ≠ 0 := one_ne_zero,
+    have := (ring_char.spec R 1).2 (by { rw hp', }),
+    norm_cast at this,
   },
-  rw hp' at h0,
-  simp at h0,
-  simp [h0, hp'] at hneq,
-  exact hneq,
+  exact hneq hp',
 end
 
 lemma power_of_prime_neq_char_is_non_zero
-(K : Type*) [field K] (n : ℕ) (q : ℕ) (a : ℕ) (hq : nat.prime q) (hn : n = q^a) (hneq : ring_char K ≠ q) : (n : K) ≠ 0 :=
+(R : Type*) [semiring R] [no_zero_divisors R] [nontrivial R]
+(n : ℕ) (q : ℕ) (a : ℕ) (hq : nat.prime q) (hn : n = q^a) (hneq : ring_char R ≠ q) : (n : R) ≠ 0 :=
 begin
-  have h := prime_neq_char_is_non_zero K q hq hneq,
+  have h := prime_neq_char_is_non_zero R q hq hneq,
   rw hn,
   field_simp [h],
 end
 
-lemma dvd_char_is_zero
-{K : Type*} [field K] {p : ℕ} (hp : ring_char K = p) (n : ℤ) (hdvd : (p : ℤ) ∣ n) : (n : K) = 0 :=
+lemma dvd_char_is_zero_N
+{R : Type*} [semiring R] {p : ℕ} (hp : ring_char R = p) (n : ℕ) (hdvd : p ∣ n) : (n : R) = 0 :=
 begin
   rw ring_char.eq_iff at hp,
-  rw @char_p.int_cast_eq_zero_iff K _ p hp n,
+  rw @char_p.cast_eq_zero_iff R _ p hp n,
+  exact hdvd,
+end
+
+lemma dvd_char_is_zero
+{R : Type*} [ring R] {p : ℕ} (hp : ring_char R = p) (n : ℤ) (hdvd : (p : ℤ) ∣ n) : (n : R) = 0 :=
+begin
+  rw ring_char.eq_iff at hp,
+  rw @char_p.int_cast_eq_zero_iff R _ p hp n,
+  exact hdvd,
+end
+
+lemma ndvd_char_is_non_zero_N
+{R : Type*} [semiring R] {p : ℕ} (hp : ring_char R = p) (n : ℕ) (hdvd : ¬ p ∣ n) : (n : R) ≠ 0 :=
+begin
+  rw ring_char.eq_iff at hp,
+  rw [ne.def, @char_p.cast_eq_zero_iff R _ p hp n],
   exact hdvd,
 end
 
 lemma ndvd_char_is_non_zero
-{K : Type*} [field K] {p : ℕ} (hp : ring_char K = p) (n : ℤ) (hdvd : ¬ (p : ℤ) ∣ n) : (n : K) ≠ 0 :=
+{R : Type*} [ring R] {p : ℕ} (hp : ring_char R = p) (n : ℤ) (hdvd : ¬ (p : ℤ) ∣ n) : (n : R) ≠ 0 :=
 begin
   rw ring_char.eq_iff at hp,
-  rw [ne.def, @char_p.int_cast_eq_zero_iff K _ p hp n],
+  rw [ne.def, @char_p.int_cast_eq_zero_iff R _ p hp n],
   exact hdvd,
 end
 
+lemma cong_char_is_eq_N'
+{R : Type*} [semiring R] {p : ℕ} (hp : ring_char R = p) (a b : ℕ) (hleq : a ≤ b) (hdvd : (p : ℤ) ∣ (b - a)) : (a : R) = (b : R) :=
+begin
+  by_cases hpzero : p = 0, {
+    rw hpzero at hdvd,
+    simp [sub_eq_zero] at hdvd,
+    rw hdvd,
+  },
+  replace hpzero := pos_iff_ne_zero.mpr hpzero,
+  zify at hpzero,
+  cases hdvd with c hdvd,
+  rw ring_char.eq_iff at hp,
+  zify at hleq,
+  replace hleq : ((b:ℤ) - a) ≥ 0 := by linarith,
+  rw hdvd at hleq,
+  simp [hpzero] at hleq,
+  lift c to ℕ using hleq,
+  replace hdvd : (b:ℤ) = a + p * c := by { rw ← hdvd, ring, },
+  norm_cast at hdvd,
+  rw hdvd,
+  transitivity (a:R) + p * c, {
+    rw (@char_p.cast_eq_zero_iff R _ p hp p).2 (dvd_refl p),
+    simp,
+  },
+  simp,
+end
+
+lemma cong_char_is_eq_N
+{R : Type*} [semiring R] {p : ℕ} (hp : ring_char R = p) (a b : ℕ) (hdvd : (p : ℤ) ∣ (b - a)) : (a : R) = (b : R) :=
+begin
+  by_cases h : b ≥ a, {
+    exact cong_char_is_eq_N' hp a b h hdvd,
+  },
+  replace h : a ≥ b := by linarith,
+  replace hdvd : (p : ℤ) ∣ (a - b) := by {
+    cases hdvd with c hdvd,
+    use -c,
+    replace hdvd : (b:ℤ) = a + p * c := by { rw ← hdvd, ring, },
+    rw hdvd,
+    ring,
+  },
+  exact (cong_char_is_eq_N' hp b a h hdvd).symm,
+end
+
 lemma cong_char_is_eq
-{K : Type*} [field K] {p : ℕ} (hp : ring_char K = p) (a b : ℤ) (hdvd : (p : ℤ) ∣ (b - a)) : (a : K) = (b : K) :=
+{R : Type*} [ring R] {p : ℕ} (hp : ring_char R = p) (a b : ℤ) (hdvd : (p : ℤ) ∣ (b - a)) : (a : R) = (b : R) :=
 begin
   rw ring_char.eq_iff at hp,
-  rw @char_p.int_coe_eq_int_coe_iff K _ p hp a b,
+  rw @char_p.int_coe_eq_int_coe_iff R _ p hp a b,
   rw int.modeq.modeq_iff_dvd,
   exact hdvd,
 end
+
+lemma ncong_char_is_neq
+{R : Type*} [ring R] {p : ℕ} (hp : ring_char R = p) (a b : ℤ) (hdvd : ¬ (p : ℤ) ∣ (b - a)) : (a : R) ≠ (b : R) :=
+begin
+  rw ring_char.eq_iff at hp,
+  rw [ne.def, @char_p.int_coe_eq_int_coe_iff R _ p hp a b],
+  rw int.modeq.modeq_iff_dvd,
+  exact hdvd,
+end
+
+section char_specific
+  variables {R : Type*} [semiring R]
+
+  lemma char_two_bit0 (hchar : ring_char R = 2) (x : R) : bit0 x = 0 := begin
+    unfold bit0,
+    transitivity (2:R) * x,
+    { rw [bit0, add_mul, one_mul], },
+    rw ring_char.eq_iff at hchar,
+    have := @char_p.cast_eq_zero R _ 2 hchar,
+    norm_cast at this,
+    rw [this, zero_mul],
+  end
+
+  lemma char_two_bit1 (hchar : ring_char R = 2) (x : R) : bit1 x = 1 := begin
+    unfold bit1,
+    rw [char_two_bit0 hchar, zero_add],
+  end
+
+  example (hchar : ring_char R = 2) : (35 : R) = 37 := begin
+    simp [char_two_bit0 hchar, char_two_bit1 hchar],
+  end
+
+  open tactic (get_local infer_type)
+  open interactive (parse)
+  open lean.parser (ident)
+  open interactive (loc.ns)
+  open interactive.types (texpr location)
+
+  -- (hchar_ : parse texpr)
+  -- hchar ← tactic.i_to_expr hchar_,
+  -- `(ring_char _ = 2) ← infer_type hchar,
+
+  /--
+  Experimental ring tactic for characteristic 2.
+  FIXME: You must provide a `hchar2 : ring_char R = 2` hypothesis.
+  -/
+  meta def tactic.interactive.ring_char2 : tactic unit :=
+  do
+    hchar ← get_local `hchar2,
+    `(ring_char _ = 2) ← infer_type hchar,
+    `[ repeat {
+      simp [char_two_bit0 hchar2, char_two_bit1 hchar2,
+      pow_zero, pow_one, zero_mul, mul_zero] <|> ring_nf }
+    ]
+
+  example (hchar2 : ring_char R = 2) : (35 : R) = 37 := begin
+    ring_char2,
+  end
+
+  lemma char_three_3_eq_0 (hchar : ring_char R = 3) : (3:R) = 0 := begin
+    have := dvd_char_is_zero_N hchar 3 (by norm_num),
+    norm_cast at this,
+    exact this,
+  end
+
+  lemma char_three_4_eq_1 (hchar : ring_char R = 3) : (4:R) = 1 := begin
+    have := cong_char_is_eq_N hchar 4 1 (by norm_num),
+    norm_cast at this,
+    exact this,
+  end
+
+  lemma char_three_5_eq_2 (hchar : ring_char R = 3) : (5:R) = 2 := begin
+    have := cong_char_is_eq_N hchar 5 2 (by norm_num),
+    norm_cast at this,
+    exact this,
+  end
+
+  example (hchar : ring_char R = 3) : (34 : R) = 37 := begin
+    simp [char_three_3_eq_0 hchar, char_three_4_eq_1 hchar, char_three_5_eq_2 hchar],
+  end
+
+  /--
+  Experimental ring tactic for characteristic 3.
+  FIXME: You must provide a `hchar3 : ring_char R = 3` hypothesis.
+  -/
+  meta def tactic.interactive.ring_char3 : tactic unit :=
+  do
+    hchar ← get_local `hchar3,
+    `(ring_char _ = 3) ← infer_type hchar,
+    `[ repeat {
+      simp [char_three_3_eq_0 hchar3, char_three_4_eq_1 hchar3, char_three_5_eq_2 hchar3,
+      pow_zero, pow_one, zero_mul, mul_zero] <|> ring_nf }
+    ]
+
+  example (hchar3 : ring_char R = 3) : (34 : R) = 37 := begin
+    ring_char3,
+  end
+
+end char_specific
