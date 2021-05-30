4f5be69fec59ce2099acd1522874b4c530389ae2
acmepjz <acme_pjz@hotmail.com>
2021-05-31 04:36:51 +0800

    WIP

--- gtm106/weierstrass_equation/group_law/add_assoc.lean
+++ gtm106/weierstrass_equation/group_law/add_assoc.lean
@@ -2,6 +2,7 @@ import algebra.field
 import gtm106.weierstrass_equation.basic
 import gtm106.weierstrass_equation.point
 import gtm106.weierstrass_equation.group_law.basic
+import gtm106.weierstrass_equation.group_law.add_assoc_lemma_1
 import tactic
 
 namespace weierstrass_equation
@@ -10,96 +11,74 @@ namespace affine_point
 
 -- copied from [Fri17] An elementary proof of the group law for elliptic curves
 
-lemma add.assoc.lemma_2_1
-{K : Type*} [field K] {E : weierstrass_equation K}
-(P1 P2 P3 : affine_plane_point K)
-(h1 : E.affine_point_on_curve P1)
-(h2 : E.affine_point_on_curve P2)
-(h3 : E.affine_point_on_curve P3)
-(hx12 : P1.x - P2.x ≠ 0)
-(hx23 : P2.x - P3.x ≠ 0)
-(hx123 : (E.add_of_affine_plane_point P1 P2).x - P3.x ≠ 0)
-(hx231 : (E.add_of_affine_plane_point P2 P3).x - P1.x ≠ 0)
-: E.add_of_affine_plane_point (E.add_of_affine_plane_point P1 P2) P3
-= E.add_of_affine_plane_point P1 (E.add_of_affine_plane_point P2 P3) :=
-begin
-  sorry,
-end
-
-lemma add.assoc.lemma_2_2
-{K : Type*} [field K] {E : weierstrass_equation K}
-(P1 P2 : affine_plane_point K)
-(h1 : E.affine_point_on_curve P1)
-(h2 : E.affine_point_on_curve P2)
-(h1not2tors : 2*P1.y + E.a1*P1.x + E.a3 ≠ 0)
-(hx12 : P1.x - P2.x ≠ 0)
-(hx112 : (E.double_of_affine_plane_point P1).x - P2.x ≠ 0)
-(hx121 : (E.add_of_affine_plane_point P1 P2).x - P1.x ≠ 0)
-: E.add_of_affine_plane_point (E.double_of_affine_plane_point P1) P2
-= E.add_of_affine_plane_point P1 (E.add_of_affine_plane_point P1 P2) :=
-begin
-  sorry,
-end
-
-lemma add.assoc.lemma_2_3
-{K : Type*} [field K] {E : weierstrass_equation K}
-(P : affine_plane_point K)
-(h : E.affine_point_on_curve P)
-(hnot2tors : 2*P.y + E.a1*P.x + E.a3 ≠ 0)
-(h2not2tors : 2*(E.double_of_affine_plane_point P).y + E.a1*(E.double_of_affine_plane_point P).x + E.a3 ≠ 0)
-(hx31 : (E.add_of_affine_plane_point (E.double_of_affine_plane_point P) P).x - P.x ≠ 0)
-(hx21 : (E.double_of_affine_plane_point P).x - P.x ≠ 0)
-: (E.double_of_affine_plane_point (E.double_of_affine_plane_point P))
-= E.add_of_affine_plane_point P (E.add_of_affine_plane_point P (E.double_of_affine_plane_point P)) :=
-begin
-  sorry,
-end
-
-lemma add.assoc.lemma_2_4
+lemma add.assoc.lemma_2_5
 {K : Type*} [field K] {E : weierstrass_equation K}
 (P1 P2 : affine_point E)
-: (P1.neg).add P2.neg = P1.neg_of_add P2 :=
+: P1.add P2 = P1.add P2.neg → P1 ≠ P1.neg → P2 = P2.neg :=
 match P1, P2 with
 | infinite, infinite := by {
-  simp [add, neg, neg_of_add],
+  simp [neg],
 }
 | infinite, (finite P2 h2) := by {
-  simp [add, neg, neg_of_add],
+  simp [neg],
 }
 | (finite P1 h1), infinite := by {
-  simp [add, neg, neg_of_add],
+  simp [neg],
 }
 | (finite P1 h1), (finite P2 h2) := by {
-  simp [add, neg],
+  intros hh1 hh2,
   by_cases hx : P1.x - P2.x = 0, {
-    have hx' : (E.neg_of_affine_plane_point P1).x - (E.neg_of_affine_plane_point P2).x = 0 := by {
-      simp only [neg_of_affine_plane_point, hx],
-    },
-    by_cases hy : P1.y + P2.y + E.a1*P1.x + E.a3 = 0, {
-      have hy' : (E.neg_of_affine_plane_point P1).y + (E.neg_of_affine_plane_point P2).y + E.a1*(E.neg_of_affine_plane_point P1).x + E.a3 = 0, {
-        replace hx := sub_eq_zero.1 hx,
-        replace hy := congr (congr_arg has_sub.sub (eq.refl P1.y)) hy.symm,
-        ring_nf at hy,
-        simp only [neg_of_affine_plane_point, hx, hy],
-        ring,
+    exfalso,
+    cases same_x_implies_same_or_neg' P1 P2 h1 h2 hx with hP hP, {
+      replace hP : finite P1 h1 = finite P2 h2 := by {
+        simp only [hP],
       },
-      simp [neg_of_add, hx, hx', hy, hy', neg],
+      rw [← hP, add.add_right_neg, add.add_to_zero_iff_neg] at hh1,
+      exact hh2 hh1,
     },
-    have hy' : (E.neg_of_affine_plane_point P1).y + (E.neg_of_affine_plane_point P2).y + E.a1*(E.neg_of_affine_plane_point P1).x + E.a3 ≠ 0, {
-      replace hx := sub_eq_zero.1 hx,
-      revert hy,
-      contrapose,
-      push_neg,
-      intro hy,
-      simp [neg_of_affine_plane_point] at hy,
-      replace hy := congr (congr_arg has_add.add (eq.refl P1.y)) hy.symm,
-      rw add_zero at hy,
-      rw [hy, hx],
-      ring,
+    replace hP : finite P1 h1 = (finite P2 h2).neg := by {
+      simp only [neg, hP],
     },
-    simp [neg_of_add, hx, hx', hy, hy', neg],
-    sorry,
+    replace hh1 := hh1.symm,
+    rw [hP, add.add_left_neg, add.add_to_zero_iff_neg, ← hP] at hh1,
+    exact hh2 hh1,
   },
+  simp [neg, neg_of_affine_plane_point.self_eq_neg_iff] at hh2,
+  simp [add, neg_of_add, neg, hx, neg_of_affine_plane_point,
+    neg_of_add_of_affine_plane_point] at hh1,
+  replace hh1 := sub_eq_zero.2 hh1.1.symm,
+  simp only [neg_of_add_of_affine_plane_point.x,
+    neg_of_add_of_affine_plane_point.A,
+    neg_of_add_of_affine_plane_point.C] at hh1,
+  field_simp [hx] at hh1,
+  replace hh1 : (2*P1.y + E.a1*P1.x + E.a3) * (2*P2.y + E.a1*P2.x + E.a3) * (P1.x - P2.x)^2 = 0 := by {
+    rw [← hh1, ← sub_eq_zero],
+    ring,
+  },
+  simp [hx, hh2] at hh1,
+  simp [neg],
+  exact (neg_of_affine_plane_point.self_eq_neg_iff E P2).2 hh1,
+}
+end
+
+lemma add.add_eq_self_iff
+{K : Type*} [field K] {E : weierstrass_equation K}
+(hns : E.non_singular')
+(P1 P2 : affine_point E)
+: P1.add P2 = P1 ↔ P2 = infinite :=
+match P1, P2 with
+| infinite, infinite := by {
+  simp [add, neg_of_add, neg],
+}
+| infinite, (finite P2 h2) := by {
+  simp [add, neg_of_add, neg],
+}
+| (finite P1 h1), infinite := by {
+  simp [add, neg_of_add, neg],
+}
+| (finite P1 h1), (finite P2 h2) := by {
+  simp only [iff_false],
+  intro h,
   sorry,
 }
 end
--- gtm106/weierstrass_equation/group_law/basic.lean
+++ gtm106/weierstrass_equation/group_law/basic.lean
@@ -47,6 +47,22 @@ def neg_of_affine_plane_point
 (P : affine_plane_point K) : affine_plane_point K
 := ⟨ P.x, -P.y - E.a1*P.x - E.a3 ⟩
 
+lemma neg_of_affine_plane_point.self_eq_neg_iff
+{K : Type*} [comm_ring K] (E : weierstrass_equation K)
+(P : affine_plane_point K)
+: P = E.neg_of_affine_plane_point P ↔ 2*P.y + E.a1*P.x + E.a3 = 0 :=
+begin
+  simp [neg_of_affine_plane_point, affine_plane_point.ext_iff],
+  split, {
+    intro h,
+    rw ← sub_eq_zero.2 h,
+    ring,
+  },
+  intro h,
+  rw [← sub_eq_zero, ← h],
+  ring,
+end
+
 @[simp]
 lemma neg_of_neg_of_affine_plane_point
 {K : Type*} [comm_ring K] (E : weierstrass_equation K)
@@ -112,9 +128,8 @@ begin
   },
   right,
   rw sub_eq_zero at hx,
-  replace hy := congr (congr_arg has_sub.sub (eq.refl P1.y)) hy.symm,
-  ring_nf at hy,
-  simp [neg_of_affine_plane_point, affine_plane_point.ext_iff, hx, hy],
+  simp [neg_of_affine_plane_point, affine_plane_point.ext_iff, hx],
+  rw [← sub_eq_zero, ← hy, hx],
   ring,
 end
 
@@ -177,6 +192,65 @@ def C : K := 2*P.y + E.a1*P.x + E.a3
 def x : K := (A/C + E.a1)*(A/C) - E.a2 - 2*P.x -- we avoid using (A/C)^2
 def y : K := P.y + (x - P.x)*(A/C) -- we avoid using B
 
+lemma C' (h : E.affine_point_on_curve P)
+: C^2 = 4*P.x^3 + E.b2*P.x^2 + 2*E.b4*P.x + E.b6 :=
+sub_eq_zero.1 begin
+  transitivity E.eval_at_affine_point P * 4, {
+    rw ← sub_eq_zero,
+    simp only [eval_at_affine_point, C, b2, b4, b6],
+    ring,
+  },
+  unfold affine_point_on_curve at h,
+  simp [h],
+end
+
+lemma x' (h : E.affine_point_on_curve P) (hC : C ≠ 0)
+: x = (P.x^4 - E.b4*P.x^2 - 2*E.b6*P.x - E.b8) / (4*P.x^3 + E.b2*P.x^2 + 2*E.b4*P.x + E.b6) :=
+sub_eq_zero.1 begin
+  simp only [← C' E P h, x],
+  field_simp [hC],
+  transitivity ((A E P + E.a1 * C E P) * A E P
+  - C E P * C E P * (E.a2 + 2 * P.x)
+  - (P.x ^ 4 - E.b4 * P.x ^ 2 - 2 * E.b6 * P.x - E.b8)) * (C E P)^2,
+  { rw ← sub_eq_zero, ring, },
+  simp [hC],
+  transitivity E.eval_at_affine_point P * (-E.a1^2 - 4*E.a2 - 8*P.x), {
+    rw ← sub_eq_zero,
+    simp only [eval_at_affine_point, A, C, b4, b6, b8],
+    ring,
+  },
+  unfold affine_point_on_curve at h,
+  simp [h],
+end
+
+end
+
+lemma C_of_neg {K : Type*} [field K] (E : weierstrass_equation K)
+(P : affine_plane_point K)
+: C E (E.neg_of_affine_plane_point P) = -C E P :=
+sub_eq_zero.1 begin
+  simp only [C, neg_of_affine_plane_point],
+  ring,
+end
+
+lemma x_of_neg {K : Type*} [field K] (E : weierstrass_equation K)
+(P : affine_plane_point K) (h : E.affine_point_on_curve P) (hC : C E P ≠ 0)
+: x E (E.neg_of_affine_plane_point P) = x E P :=
+begin
+  rw x' E P h hC,
+  rw x' E (E.neg_of_affine_plane_point P) (by simp [h]) (by simp [C_of_neg E P, hC]),
+  simp only [neg_of_affine_plane_point],
+end
+
+lemma y_of_neg {K : Type*} [field K] (E : weierstrass_equation K)
+(P : affine_plane_point K) (h : E.affine_point_on_curve P) (hC : C E P ≠ 0)
+: y E (E.neg_of_affine_plane_point P) = - y E P - E.a1 * x E P - E.a3 :=
+sub_eq_zero.1 begin
+  simp only [y, x_of_neg E P h hC, C_of_neg],
+  simp only [neg_of_affine_plane_point, A],
+  field_simp [hC],
+  simp only [C],
+  ring,
 end
 
 end neg_of_double_of_affine_plane_point
@@ -189,6 +263,20 @@ def neg_of_double_of_affine_plane_point
 (P : affine_plane_point K) : affine_plane_point K :=
 ⟨ neg_of_double_of_affine_plane_point.x E P,
   neg_of_double_of_affine_plane_point.y E P ⟩
+
+namespace neg_of_double_of_affine_plane_point
+
+lemma neg {K : Type*} [field K] (E : weierstrass_equation K)
+(P : affine_plane_point K) (h : E.affine_point_on_curve P) (hC : C E P ≠ 0)
+: E.neg_of_double_of_affine_plane_point (E.neg_of_affine_plane_point P)
+= E.neg_of_affine_plane_point (E.neg_of_double_of_affine_plane_point P) :=
+begin
+  simp only [neg_of_double_of_affine_plane_point, affine_plane_point.ext_iff,
+    x_of_neg E P h hC, y_of_neg E P h hC],
+  simp [neg_of_affine_plane_point],
+end
+
+end neg_of_double_of_affine_plane_point
 
 lemma affine_point_on_curve.neg_of_double
 {K : Type*} [field K] (E : weierstrass_equation K)
@@ -256,6 +344,92 @@ end
 
 end
 
+lemma A_of_comm {K : Type*} [field K]
+(P1 P2 : affine_plane_point K)
+: A P2 P1 = - A P1 P2 := by simp [A]
+
+lemma C_of_comm {K : Type*} [field K]
+(P1 P2 : affine_plane_point K)
+: C P2 P1 = - C P1 P2 := by simp [C]
+
+lemma A_over_C_of_comm {K : Type*} [field K]
+(P1 P2 : affine_plane_point K)
+: A P2 P1 / C P2 P1 = A P1 P2 / C P1 P2 :=
+begin
+  rw [A_of_comm P1 P2, C_of_comm P1 P2],
+  by_cases hC : C P1 P2 = 0, {
+    simp [hC],
+  },
+  field_simp [hC],
+end
+
+lemma x_of_comm {K : Type*} [field K] (E : weierstrass_equation K)
+(P1 P2 : affine_plane_point K)
+: x E P2 P1 = x E P1 P2 :=
+sub_eq_zero.1 begin
+  simp only [x, A_over_C_of_comm P1 P2],
+  ring,
+end
+
+lemma y_of_comm {K : Type*} [field K] (E : weierstrass_equation K)
+(P1 P2 : affine_plane_point K)
+(hx : P1.x - P2.x ≠ 0)
+: y E P2 P1 = y E P1 P2 :=
+begin
+  simp only [y, y' E P1 P2 hx, x_of_comm E P1 P2, A_over_C_of_comm P1 P2],
+end
+
+lemma A_of_neg {K : Type*} [field K] (E : weierstrass_equation K)
+(P1 P2 : affine_plane_point K)
+: A (E.neg_of_affine_plane_point P1) (E.neg_of_affine_plane_point P2)
+= - A P1 P2 - E.a1 * C P1 P2 :=
+begin
+  simp [A, C, neg_of_affine_plane_point],
+  ring,
+end
+
+lemma C_of_neg {K : Type*} [field K] (E : weierstrass_equation K)
+(P1 P2 : affine_plane_point K)
+: C (E.neg_of_affine_plane_point P1) (E.neg_of_affine_plane_point P2)
+= C P1 P2 :=
+begin
+  simp [C, neg_of_affine_plane_point],
+end
+
+lemma A_over_C_of_neg {K : Type*} [field K] (E : weierstrass_equation K)
+(P1 P2 : affine_plane_point K)
+(hx : P1.x - P2.x ≠ 0)
+: A (E.neg_of_affine_plane_point P1) (E.neg_of_affine_plane_point P2)
+/ C (E.neg_of_affine_plane_point P1) (E.neg_of_affine_plane_point P2)
+= - A P1 P2 / C P1 P2 - E.a1 :=
+begin
+  simp [A_of_neg, C_of_neg],
+  change C P1 P2 ≠ 0 at hx,
+  field_simp [hx],
+  ring,
+end
+
+lemma x_of_neg {K : Type*} [field K] (E : weierstrass_equation K)
+(P1 P2 : affine_plane_point K)
+(hx : P1.x - P2.x ≠ 0)
+: x E (E.neg_of_affine_plane_point P1) (E.neg_of_affine_plane_point P2) = x E P1 P2 :=
+sub_eq_zero.1 begin
+  simp only [x, A_over_C_of_neg E P1 P2 hx],
+  simp only [neg_of_affine_plane_point],
+  ring,
+end
+
+lemma y_of_neg {K : Type*} [field K] (E : weierstrass_equation K)
+(P1 P2 : affine_plane_point K)
+(hx : P1.x - P2.x ≠ 0)
+: y E (E.neg_of_affine_plane_point P1) (E.neg_of_affine_plane_point P2)
+= - y E P1 P2 - E.a1 * x E P1 P2 - E.a3 :=
+sub_eq_zero.1 begin
+  simp only [y, x_of_neg E P1 P2 hx, A_over_C_of_neg E P1 P2 hx],
+  simp only [neg_of_affine_plane_point],
+  ring,
+end
+
 end neg_of_add_of_affine_plane_point
 
 /--
@@ -267,33 +441,31 @@ def neg_of_add_of_affine_plane_point
 ⟨ neg_of_add_of_affine_plane_point.x E P1 P2,
   neg_of_add_of_affine_plane_point.y E P1 P2 ⟩
 
-lemma neg_of_add_of_affine_plane_point.comm
+namespace neg_of_add_of_affine_plane_point
+
+lemma comm
 {K : Type*} [field K] (E : weierstrass_equation K)
 (P1 P2 : affine_plane_point K)
 (hx : P1.x - P2.x ≠ 0)
 : E.neg_of_add_of_affine_plane_point P1 P2 = E.neg_of_add_of_affine_plane_point P2 P1 :=
 begin
-  simp only [neg_of_add_of_affine_plane_point, affine_plane_point.ext_iff],
-  have h1 : neg_of_add_of_affine_plane_point.A P1 P2 / neg_of_add_of_affine_plane_point.C P1 P2
-  = neg_of_add_of_affine_plane_point.A P2 P1 / neg_of_add_of_affine_plane_point.C P2 P1 := by {
-    simp only [neg_of_add_of_affine_plane_point.A, neg_of_add_of_affine_plane_point.C],
-    by_cases hx : P1.x - P2.x = 0, {
-      simp [sub_eq_zero.1 hx],
-    },
-    have hx' := sub_ne_zero.2 (sub_ne_zero.1 hx).symm,
-    field_simp [hx, hx'],
-    ring,
-  },
-  have h2 : neg_of_add_of_affine_plane_point.x E P1 P2 = neg_of_add_of_affine_plane_point.x E P2 P1 := by {
-    unfold neg_of_add_of_affine_plane_point.x,
-    rw h1,
-    ring,
-  },
-  use h2,
-  rw [neg_of_add_of_affine_plane_point.y' E P1 P2 hx, h2],
-  unfold neg_of_add_of_affine_plane_point.y,
-  rw h1,
+  simp [neg_of_add_of_affine_plane_point, affine_plane_point.ext_iff,
+    x_of_comm E P1 P2, y_of_comm E P1 P2 hx],
 end
+
+lemma neg
+{K : Type*} [field K] (E : weierstrass_equation K)
+(P1 P2 : affine_plane_point K)
+(hx : P1.x - P2.x ≠ 0)
+: E.neg_of_add_of_affine_plane_point (E.neg_of_affine_plane_point P1) (E.neg_of_affine_plane_point P2)
+= E.neg_of_affine_plane_point (E.neg_of_add_of_affine_plane_point P1 P2) :=
+begin
+  simp only [neg_of_add_of_affine_plane_point, affine_plane_point.ext_iff,
+    x_of_neg E P1 P2 hx, y_of_neg E P1 P2 hx],
+  simp [neg_of_affine_plane_point],
+end
+
+end neg_of_add_of_affine_plane_point
 
 lemma affine_point_on_curve.neg_of_add
 {K : Type*} [field K] (E : weierstrass_equation K)
@@ -513,6 +685,102 @@ match P with
   dsimp only,
   have h : P.y + (-P.y - E.a1 * P.x - E.a3) + E.a1 * P.x + E.a3 = 0 := by ring,
   simp [h, neg],
+}
+end
+
+lemma add.neg_add_neg
+{K : Type*} [field K] {E : weierstrass_equation K}
+(P1 P2 : affine_point E)
+: (P1.neg).add P2.neg = P1.neg_of_add P2 :=
+match P1, P2 with
+| infinite, infinite := by {
+  simp [add, neg, neg_of_add],
+}
+| infinite, (finite P2 h2) := by {
+  simp [add, neg, neg_of_add],
+}
+| (finite P1 h1), infinite := by {
+  simp [add, neg, neg_of_add],
+}
+| (finite P1 h1), (finite P2 h2) := by {
+  simp [add, neg],
+  by_cases hx : P1.x - P2.x = 0, {
+    have hx' : (E.neg_of_affine_plane_point P1).x - (E.neg_of_affine_plane_point P2).x = 0 := by {
+      simp only [neg_of_affine_plane_point, hx],
+    },
+    by_cases hy : P1.y + P2.y + E.a1*P1.x + E.a3 = 0, {
+      have hy' : (E.neg_of_affine_plane_point P1).y + (E.neg_of_affine_plane_point P2).y + E.a1*(E.neg_of_affine_plane_point P1).x + E.a3 = 0, {
+        replace hx := sub_eq_zero.1 hx,
+        replace hy := congr_arg (λ x, -x) hy.symm,
+        simp only [neg_zero] at hy,
+        simp only [neg_of_affine_plane_point, hx, hy],
+        ring,
+      },
+      simp [neg_of_add, hx, hx', hy, hy', neg],
+    },
+    have hP := same_x_implies_same_or_neg P1 P2 h1 h2 hx,
+    simp [hy] at hP,
+    replace hP : P1 = P2 := by {
+      simp [affine_plane_point.ext_iff, sub_eq_zero.1 hx, sub_eq_zero.1 hP],
+    },
+    have hy' : (E.neg_of_affine_plane_point P1).y + (E.neg_of_affine_plane_point P2).y + E.a1*(E.neg_of_affine_plane_point P1).x + E.a3 ≠ 0, {
+      replace hx := sub_eq_zero.1 hx,
+      revert hy,
+      contrapose,
+      push_neg,
+      intro hy,
+      simp [neg_of_affine_plane_point] at hy,
+      replace hy := congr (congr_arg has_add.add (eq.refl P1.y)) hy.symm,
+      rw add_zero at hy,
+      rw [hy, hx],
+      ring,
+    },
+    simp [neg_of_add, hx, hx', hy, hy', neg],
+    rw [← hP, ← two_mul] at hy,
+    simp [neg_of_double_of_affine_plane_point.neg E P1 h1 hy],
+  },
+  have hx' : (E.neg_of_affine_plane_point P1).x - (E.neg_of_affine_plane_point P2).x ≠ 0 := by {
+    simp only [neg_of_affine_plane_point], exact hx,
+  },
+  simp [neg_of_add, hx, hx', neg, neg_of_add_of_affine_plane_point.neg E P1 P2 hx],
+}
+end
+
+lemma add.add_to_zero_iff_neg
+{K : Type*} [field K] {E : weierstrass_equation K}
+(P1 P2 : affine_point E)
+: P1.add P2 = infinite ↔ P2 = P1.neg :=
+match P1, P2 with
+| infinite, infinite := by {
+  simp [add, neg_of_add, neg],
+}
+| infinite, (finite P2 h2) := by {
+  simp [add, neg_of_add, neg],
+}
+| (finite P1 h1), infinite := by {
+  simp [add, neg_of_add, neg],
+}
+| (finite P1 h1), (finite P2 h2) := by {
+  by_cases hx : P1.x - P2.x = 0, {
+    by_cases hy : P1.y + P2.y + E.a1*P1.x + E.a3 = 0, {
+      simp [add, neg_of_add, neg, hx, hy],
+      replace hx := (sub_eq_zero.1 hx).symm,
+      simp [neg_of_affine_plane_point, affine_plane_point.ext_iff, hx],
+      rw [← sub_eq_zero, ← hy],
+      ring,
+    },
+    simp [add, neg_of_add, neg, hx, hy],
+    replace hx := (sub_eq_zero.1 hx).symm,
+    simp [neg_of_affine_plane_point, affine_plane_point.ext_iff, hx],
+    revert hy,
+    contrapose,
+    push_neg,
+    intro hy,
+    rw hy,
+    ring,
+  },
+  simp [add, neg_of_add, neg, hx],
+  simp [neg_of_affine_plane_point, affine_plane_point.ext_iff, (sub_ne_zero.1 hx).symm],
 }
 end
 
